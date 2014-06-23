862a1e607ab52f2bac4064c4a28912ee879875ad
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2014-06-23 18:55:32 +0800

    test word2vec

---
+++ src/model_word2vec/Test.java
@@ -0,0 +1,16 @@
+package model_word2vec;
+
+import java.io.IOException;
+
+import org.ansj.splitWord.analysis.ToAnalysis;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+//        Word2VEC w1 = new Word2VEC() ;
+//        w1.loadJavaModel("library/vector.mod") ;
+//        
+//        System.out.println(w1.distance("奥尼尔"));
+        
+//        System.out.println(ToAnalysis.parse("汤姆克鲁斯的英文名字很苦"));
+    }
+}
