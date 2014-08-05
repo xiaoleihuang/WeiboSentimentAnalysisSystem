f718e23c1cfab98b571e4a7cf73ef480a6cc338f
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2014-08-06 07:38:27 +0800

    modify

--- src/result_chart/CreateXYLineChart.java
+++ src/result_chart/CreateXYLineChart.java
@@ -6,6 +6,8 @@ import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Shape;
 import java.awt.Stroke;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,7 +21,9 @@ import org.jCharts.chartData.ChartDataException;
 import org.jCharts.chartData.DataSeries;
 import org.jCharts.encoders.JPEGEncoder;
 import org.jCharts.properties.AxisProperties;
+import org.jCharts.properties.AxisTypeProperties;
 import org.jCharts.properties.ChartProperties;
+import org.jCharts.properties.DataAxisProperties;
 import org.jCharts.properties.LegendProperties;
 import org.jCharts.properties.LineChartProperties;
 import org.jCharts.properties.PointChartProperties;
@@ -28,35 +32,38 @@ import org.jCharts.properties.util.ChartFont;
 import org.jCharts.test.TestDataGenerator;
 import org.jCharts.types.ChartType;
 
-public class CreateLineChart extends JFrame{
-	private static final long serialVersionUID = 7900314316647197052L;
-	JPanel panel;
-	AxisChart axisChart;
+
+/**
+ * Create a Line Chart
+ * @author xiaolei
+ */
+public class CreateXYLineChart extends JFrame {
+	private static final long serialVersionUID = 8470483224989800067L;
+	public JPanel panel;
+	public AxisChart axisChart;
 	
+	private class CloseActionListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e){
+			CreateXYLineChart.this.dispose();
+		}
+	}
 	/**
-	 * Constructor for create a line chart
-	 * @param data XY Line Chart's data
-	 * @param legendLabels the array of legends' labels
-	 * @param LineStrokes the array of Strokes types. LineChartProperties.DEFAULT_LINE_STROKE
-	 * @param LineShapes the array of Line points' shape types. PointChartProperties.SHAPE_TRIANGLE
-	 * @param xAxisLabels the labels list on x axis
-	 * @param xAxisTitle the title of x axis
-	 * @param yAxisTitle the title of y axis
+	 * Constructor
 	 * @throws ChartDataException
 	 * @throws PropertyException
 	 * @throws IOException
 	 */
-	public CreateLineChart(double[][] data,String[] legendLabels,
-			Stroke[] LineStrokes,Shape[] LineShapes,String[] xAxisLabels,
-			String xAxisTitle,String yAxisTitle
-			) throws ChartDataException, PropertyException,IOException{
+	public CreateXYLineChart(String[] xAxisLabels,String xAxisTitle,String yAxisTitle,double[][] data) throws ChartDataException, PropertyException, IOException{
 		this.setSize(1000, 1000);
 		this.panel=new JPanel(true);
+		this.panel.setAutoscrolls(true);
+		this.panel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		this.panel.setSize(500,500);
 		this.getContentPane().add(this.panel);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		this.addWindowListener(new CloseActionListener());
+		
 		//set the title of the line graph
 		String title="Line Graph";
 		
@@ -75,25 +82,35 @@ public class CreateLineChart extends JFrame{
 		//Axis Properties
 		AxisProperties axisProperties = new AxisProperties( false );
 		//AxisProperties
-		ChartFont axisScaleFont = new ChartFont(new Font( "TimesRoman",Font.BOLD, 18),Color.black );
+		ChartFont axisScaleFont = new ChartFont(new Font( "Georgia Negreta cursiva",Font.PLAIN, 23),Color.black );
 		axisProperties.getXAxisProperties().setScaleChartFont(axisScaleFont);
 		axisProperties.getYAxisProperties().setScaleChartFont( axisScaleFont );
-		ChartFont axisTitleFont = new ChartFont( new Font( "TimesRoman", Font.BOLD, 18), Color.black );
+		ChartFont axisTitleFont = new ChartFont( new Font( "Arial Narrow", Font.BOLD, 14 ), Color.black );
 		axisProperties.getXAxisProperties().setTitleChartFont( axisTitleFont );
 		axisProperties.getYAxisProperties().setTitleChartFont( axisTitleFont );
+		axisProperties.getYAxisProperties().setShowTicks(AxisTypeProperties.TICKS_ONLY_WITH_LABELS);
+		//adjust y axis scale
+		DataAxisProperties dataAxisProperties= (DataAxisProperties) axisProperties.getYAxisProperties();
+		dataAxisProperties.setRoundToNearest( -2 ); 
 		
-		LineChartProperties lineChartProperties = new LineChartProperties(LineStrokes,LineShapes);
+		Stroke[] strokes= { LineChartProperties.DEFAULT_LINE_STROKE};
+		Shape[] shapes= { PointChartProperties.SHAPE_TRIANGLE};
+		LineChartProperties lineChartProperties = new LineChartProperties(strokes,shapes);
 		
 		DataSeries dataSeries = new DataSeries( xAxisLabels, xAxisTitle, yAxisTitle,title );
 		
-		Paint[] paints= TestDataGenerator.getRandomPaints(legendLabels.length);
-		AxisChartDataSet acds = new AxisChartDataSet(data, legendLabels, paints,ChartType.SCATTER_PLOT, lineChartProperties );
-		dataSeries.addIAxisPlotDataSet(acds);
 		
+		//Test
+//		double[][] data= TestDataGenerator.getRandomNumbers( 1, 7, 0, 1 );
+
+		String[] legendLabels= { "Line" };
+		Paint[] paints= TestDataGenerator.getRandomPaints( 1 );
+		AxisChartDataSet acds = new AxisChartDataSet(data, legendLabels, paints,ChartType.LINE, lineChartProperties );
+		dataSeries.addIAxisPlotDataSet(acds);
 		axisChart=new AxisChart(dataSeries, chartProperties, axisProperties,legendProperties, 980, 720);
 		axisChart.setGraphics2D((Graphics2D) this.panel.getGraphics());
 		axisChart.render();
-	} 
+	}
 	
 	/**
 	 * Save Image 2 user appointed file path and name
@@ -106,16 +123,11 @@ public class CreateLineChart extends JFrame{
 		JPEGEncoder.encode(axisChart, 1, new FileOutputStream(new File(name)));
 	}
 	
-	public static void main(String[] args) throws ChartDataException, PropertyException, IOException {
-		// TODO Auto-generated method stub
-		double[][] data=TestDataGenerator.getRandomNumbers( 1, 6, 20, 100 );
-		String[] legendLabels= { "Predict" };
+	public static void main(String[] args) throws ChartDataException, PropertyException, IOException{
+		double[][] data={{0.09,0.1,0.2,0.3,0.4,0.5}};
 		String[] xAxisLabels= { "100", "200", "300", "400", "500", "600"};
 		String xAxisTitle= "Topics";
 		String yAxisTitle= "Coefficience";
-		Stroke[] strokes= { LineChartProperties.DEFAULT_LINE_STROKE };
-		Shape[] shapes= { PointChartProperties.SHAPE_TRIANGLE };
-		
-		new CreateLineChart(data, legendLabels, strokes, shapes, xAxisLabels, xAxisTitle, yAxisTitle);
+		new CreateXYLineChart(xAxisLabels, yAxisTitle, yAxisTitle, data);
 	}
 }
