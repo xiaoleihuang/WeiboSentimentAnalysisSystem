651aacaef26b90709d24b21b53f67b7dadcd9510
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2014-08-18 18:24:42 +0800

    modify

--- src/image_wordcloud/TestOpenCloud.java
+++ src/image_wordcloud/TestOpenCloud.java
@@ -1,4 +1,5 @@
 package image_wordcloud;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -34,14 +35,16 @@ public class TestOpenCloud {
         Cloud cloud = new Cloud();
         Random random = new Random();
         for (String s : WORDS) {
-            for (int i = random.nextInt(50); i > 0; i--) {
+            for (int i = random.nextInt(150); i > 0; i--) {
                 cloud.addTag(s);
             }
         }
         for (Tag tag : cloud.tags()) {
             final JLabel label = new JLabel(tag.getName());
             label.setOpaque(false);
-            label.setFont(label.getFont().deriveFont((float) tag.getWeight() * 10));
+            label.setFont(label.getFont().deriveFont((float) tag.getWeight() * 20));
+            if(tag.getName().contains("死")||tag.getName().contains("抑郁"))
+            	label.setForeground(Color.red);
             panel.add(label);
         }
         
@@ -52,7 +55,7 @@ public class TestOpenCloud {
         Container c = panel.getRootPane();
         BufferedImage im = new BufferedImage(c.getWidth(), c.getHeight(), BufferedImage.TYPE_INT_ARGB);
         c.paint(im.getGraphics());
-//        ImageIO.write(im, "PNG", new File("shot.png"));
+        ImageIO.write(im, "PNG", new File("word cloud.png"));
     }
     
     public static void saveImage(JPanel panel) throws IOException{
@@ -77,13 +80,15 @@ public class TestOpenCloud {
                 	GetAllWeiboPosts posts=new GetAllWeiboPosts("/home/xiaolei/Desktop/dataset/suicide/Segmentedall.txt");
                 	List<OneWeibo> list=posts.getList();
                 	List<String> ws=new ArrayList<String>();
+
                 	for(OneWeibo entry:list){
                 		String[] words=entry.getContent().split(" ");
                 		for(String w:words){
                 			w=w.trim();
                 			
                 			if(w!=""&&w!=null&&!w.contains(" ")&&w!="\n"&&!w.contains("我")&&!w.contains("你")&&!w.contains("了")
-                					&&!w.contains("都")&&!w.contains("就")&&!w.contains("也")&&!w.contains("是")&&!w.contains("有")){
+                					&&!w.contains("都")&&!w.contains("就")&&!w.contains("也")&&!w.contains("是")&&!w.contains("有")
+                					&&!w.contains("啊")){
                 			System.out.print(w);
                 			ws.add(w);
                 			}
@@ -101,5 +106,4 @@ public class TestOpenCloud {
             }
         });
     }
-    
 }
