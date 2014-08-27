96c6646fe697cb96e65fbedfed72f1d4212bd7e9
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2014-08-27 10:41:39 +0800

    modify

---
+++ src/retrieval_extractor/FanJianTransform.java
@@ -0,0 +1,21 @@
+package retrieval_extractor;
+/**
+ * Transform Traditional Chinese to Simplified Chinese
+ * @author xiaolei
+ */
+public class FanJianTransform {
+	static{
+		
+	}	public static String transformSentence(String sentence){
+		return sentence;
+	}
+	public static String transformWord(String word){
+		return word;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
