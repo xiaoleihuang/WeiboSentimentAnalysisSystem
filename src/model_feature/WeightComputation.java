e803ac70e65ce5c1a212b315e235af860f903f42
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2014-10-18 19:46:53 +0800

    modify

--- src/model_feature/WeightComputation.java
+++ src/model_feature/WeightComputation.java
@@ -4,54 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import retrieval_extractor.GetAllWeiboPosts;
+import retrieval_extractor.OneWeibo;
 
 /**
  * give a weight to the feature to solve imbalanced problems
  * @author Xiaolei Huang
- *
  */
 public class WeightComputation {
 	public static List<String> contents=new ArrayList<String>();
+	public static GetAllWeiboPosts all;
 	static{
-		GetAllWeiboPosts all=new GetAllWeiboPosts("./resource/Segmentedall.txt");
-		for(int i=0;i<all.getList().size();i++){
-			contents.add(all.getList().get(i).getContent());
-		}
+		all=new GetAllWeiboPosts("./resource/Segmentedall.txt");
 	}
 	
 	/**
 	 * Assign initial weight to feature
 	 * @param ratio
-	 * @return
+	 * @return weight
 	 */
-	public double GetInitialWeight(double ratio){
-		double weight= 10*Math.log(ratio);
+	public double GetInitialWeight(double ratio,String word){		
+		double countSuicide=0.0,countNoneSuicide=0.0;
+		for(OneWeibo post:all.getList()){
+			if(post.getContent().contains(word)&&Integer.parseInt(post.getSuicide())==1)
+				countSuicide++;
+			else
+				countNoneSuicide++;
+		}
+		double weight= 10*Math.log(countSuicide+1/countNoneSuicide)*ratio;
 		return weight;
 	}
 	
 	/**
-	 * Count word frequency in Suicidal posts
-	 * @return count
+	 * Compute the ratio of suicidal posts and none suicidal posts
+	 * @return ratio
 	 */
-	public double findSuicidalCount(String word){
-		return 0.0;
-	}
-	
-	/**
-	 * Count word frequency in None Suicidal posts
-	 * @return count
-	 */
-	public double findNoneSuicidalWordCound(String word){
-		return 0.0;
-	}
-	
-	/**
-	 * Assign weight to value
-	 * @return weight
-	 */
-	public double AssignWeight(){
-		
-		return 0.0;
+	public double getRatio(){
+		double ratio=0.0,SuicidePostCount=0.0,NoneSuicidalPostCount=0.0;
+		for(OneWeibo post:all.getList()){
+			if(Integer.parseInt(post.getSuicide())==1){
+				SuicidePostCount++;
+			}
+			else
+				NoneSuicidalPostCount++;
+		}
+		ratio=NoneSuicidalPostCount/SuicidePostCount;
+		return ratio;
 	}
 	
 	public static void main(String[] args) {
