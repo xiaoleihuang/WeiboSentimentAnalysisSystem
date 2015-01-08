0c5e5daa6547a6bc85beff9536dc94d341efeac5
xiaoleihuang <xiaoleihuangccnu@gmail.com>
2015-01-08 12:22:14 +0800

    modify

--- src/crawler/crawler/WeiboMetaFeaturesCrawler.java
+++ src/crawler/crawler/WeiboMetaFeaturesCrawler.java
@@ -5,11 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JOptionPane;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 /**
  * crawl user's comments, favorite, reposts
@@ -33,8 +38,8 @@ public class WeiboMetaFeaturesCrawler {
 		}
 	}
 	
-	private HtmlUnitDriver unit;
-	private Login login;
+	private HtmlUnitDriver driver;
+	private final String url="http://login.weibo.cn/login/?ns=1&revalid=2&backURL=http%3A%2F%2Fweibo.cn%2F&backTitle=%CE%A2%B2%A9&vt=";
 	
 	/**
 	 *  it will direct driver to http://weibo.cn/META/ID.
@@ -43,8 +48,37 @@ public class WeiboMetaFeaturesCrawler {
 	 * @throws IOException
 	 */
 	public WeiboMetaFeaturesCrawler() throws IOException{
-		login=new Login(WeiboMetaFeaturesCrawler.username, WeiboMetaFeaturesCrawler.password);
-		unit=login.getDriver();
+		driver = new HtmlUnitDriver(BrowserVersion.INTERNET_EXPLORER_10);
+		driver.get(url);
+		System.err.println("Page's Title "+driver.getTitle());
+		
+		//Use name and password to login WeiBo Account
+		try{
+			driver.findElement(By.name("mobile")).sendKeys(username);
+			driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			System.err.println("Page's Title "+driver.getTitle());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		if(driver.getTitle().contains("我的首页")){
+			System.err.println("login success");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "login failed, we will try again One Time");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			try {
+				driver.get(url);
+				driver.findElement(By.name("mobile")).sendKeys(username);
+				driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+				driver.findElement(By.xpath("//input[@type='submit']")).click();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 	
 	/**
@@ -54,11 +88,11 @@ public class WeiboMetaFeaturesCrawler {
 	 * @return 
 	 */
 	public List<WebElement> extractComments(String weiboID,String uid){
-		this.unit.get("http://weibo.cn/comment/"+weiboID+"?&uid="+uid);
+		this.driver.get("http://weibo.cn/comment/"+weiboID+"?&uid="+uid);
 		
 		//indicate the number of pages
 		int page=0;
-		WebElement pageElement=unit.findElement(By.id("pagelist"));
+		WebElement pageElement=driver.findElement(By.id("pagelist"));
 		if(pageElement!=null){
 			page=Integer.parseInt(pageElement.findElement(By.name("mp")).getAttribute("value"));
 		}
@@ -68,11 +102,11 @@ public class WeiboMetaFeaturesCrawler {
 		
 		if(page!=0)
 			for(int i=1;i<=page;i++){
-				unit.get("http://weibo.cn/comment/"+weiboID+"?uid="+uid+"&page="+i);
-				elements.addAll(unit.findElements(By.className("c")));
+				driver.get("http://weibo.cn/comment/"+weiboID+"?uid="+uid+"&page="+i);
+				elements.addAll(driver.findElements(By.className("c")));
 			}
 		else{
-			elements=unit.findElements(By.className("c"));
+			elements=driver.findElements(By.className("c"));
 		}
 		
 		//filter process
@@ -92,25 +126,25 @@ public class WeiboMetaFeaturesCrawler {
 	 * @return 
 	 */
 	public List<WebElement> extractFavorite(String weiboID){
-		this.unit.navigate().to("http://weibo.cn/attitude/"+weiboID);
+		this.driver.navigate().to("http://weibo.cn/attitude/"+weiboID);
 		
 		//indicate the number of pages
 		int page=0;
-		System.out.println(unit.getCurrentUrl());
-		WebElement pageElement=unit.findElement(By.className("pa"));
+		System.out.println(driver.getCurrentUrl());
+		WebElement pageElement=driver.findElement(By.className("pa"));
 		if(pageElement!=null){
 			page=Integer.parseInt(pageElement.findElement(By.name("mp")).getAttribute("value"));
 		}
 		
-		System.err.println(unit.getPageSource());
+		System.err.println(driver.getPageSource());
 		
-		List<WebElement> elements=unit.findElements(By.className("c"));;
+		List<WebElement> elements=driver.findElements(By.className("c"));;
 				
 		//iterate to read every elements
 		if(page!=0){
 			for(int i=2;i<=page;i++){
-				unit.get("http://weibo.cn/attitude/"+weiboID+"?rl=1&page="+i);
-				elements.addAll(unit.findElements(By.className("c")));
+				driver.get("http://weibo.cn/attitude/"+weiboID+"?rl=1&page="+i);
+				elements.addAll(driver.findElements(By.className("c")));
 			}
 		}
 		
@@ -120,7 +154,7 @@ public class WeiboMetaFeaturesCrawler {
 //				elements.remove(e);
 //				continue;
 //			}
-			System.err.println(e.getText());
+			System.err.println(e);
 		}
 		return elements;
 	}
@@ -132,11 +166,11 @@ public class WeiboMetaFeaturesCrawler {
 	 * @return 
 	 */
 	public List<WebElement> extractRepost(String weiboID){
-		this.unit.get("http://weibo.cn/repost/"+weiboID);
+		this.driver.get("http://weibo.cn/repost/"+weiboID);
 		
 		//indicate the number of pages
 		int page=0;
-		WebElement pageElement=unit.findElement(By.id("pagelist"));
+		WebElement pageElement=driver.findElement(By.id("pagelist"));
 		if(pageElement!=null){
 			page=Integer.parseInt(pageElement.findElement(By.name("mp")).getAttribute("value"));
 		}
@@ -146,11 +180,11 @@ public class WeiboMetaFeaturesCrawler {
 		//iterate to read every elements
 		if(page!=0)
 			for(int i=1;i<=page;i++){
-				unit.get("http://weibo.cn/repost/"+weiboID+"&page="+i);
-				elements.addAll(unit.findElements(By.className("c")));
+				driver.get("http://weibo.cn/repost/"+weiboID+"&page="+i);
+				elements.addAll(driver.findElements(By.className("c")));
 			}
 		else{
-			elements=unit.findElements(By.className("c"));
+			elements=driver.findElements(By.className("c"));
 		}
 		
 		//filter process
@@ -167,16 +201,15 @@ public class WeiboMetaFeaturesCrawler {
 	 * @return driver
 	 */
 	public HtmlUnitDriver getDriver(){
-		return this.unit;
+		return this.driver;
 	}
 	
 	/**
 	 * Close the driver
 	 */
 	public void close(){
-		login=null;
-		unit.quit();
-		unit.close();
+		driver.quit();
+		driver.close();
 	}
 	
 	/**
